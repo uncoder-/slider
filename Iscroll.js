b9d4db080ed6c719006924457f405bd214905457
uncoder <huiqiang.shi@wenba100.com>
2016-09-23 18:34:56 +0800

    修改

--- Iscroll.js
+++ Iscroll.js
@@ -1,12 +1,45 @@
+/**
+ * 首先明确的是touch是分4个事件的，而且对于move，cancel这两个（start触发它们不一定触发）
+ * 
+ */
 
 // 自执行函数隔离全局作用域
 (function(window, document, Math){
+
 	// 创建内部自用的公共方法（和scroll无关的，写在原型对象里也可以的）
 	var until = (function(){
 		var me = {};
+		var _elementStyle = document.createElement('div').style;
+		var _vendor = (function () {
+			var vendors = ['t', 'webkitT', 'MozT', 'msT', 'OT'];
+			var transform;
+			var l = vendors.length;
+			for ( var i = 0; i < l; i++ ) {
+				transform = vendors[i] + 'ransform';
+				if (transform in _elementStyle){
+					return vendors[i].substr(0, vendors[i].length-1);
+				} 
+			}
+			return false;
+		})();
+		function _prefixStyle (style) {
+			if ( _vendor === false ) return false;
+			if ( _vendor === '' ) return style;
+			return _vendor + style.charAt(0).toUpperCase() + style.substr(1);
+		}
+		// 获取css的兼容性前缀
+		var _transform = _prefixStyle('transform');
+		var _transition = _prefixStyle('transition');
+		var _translate3d = _prefixStyle('translate3d');
+		me.transform = _transform;
+		me.transition = _transition;
+		me.translate3d = _translate3d;
+		// 获取当前时间
 		me.getTime = Date.now || function getTime () { return new Date().getTime(); };
+
 		return me;
 	})();
+
 	// 定义滚动对象的构造函数,首字母大写
 	function Iscroll(opts) {
 		// 容器元素
@@ -14,15 +47,17 @@
 		// 滚动元素
 		this.scroller = this.wrapEl.children[0];
 		// 设置默认参数
-		this.scroller.style.transform = 'translate3d(0px,0px,0px)';
+		this.scroller['style'][until['']+'transform'] = 'translate3d(0px,0px,0px)';
 		// 容器元素高度
 		var containerHeight = this.wrapEl.offsetHeight;
 		// 滚动元素的高度
 		var scrollerHeight = this.scroller.offsetHeight;
 		// 可以滚动的高度
 		this.scrollHeight = containerHeight-scrollerHeight < 0 ? (containerHeight-scrollerHeight):0;
+		this.refresh();
 		this._init();
 	}
+
 	// 覆盖原型
 	Iscroll.prototype = {
 		_init: function(el) {
@@ -34,6 +69,7 @@
 			scroller.addEventListener('touchstart', this, false);
 			scroller.addEventListener('touchmove', this, false);
 			scroller.addEventListener('touchend', this, false);
+			scroller.addEventListener('touchcancel', this, false);
 		},
 		_start:function(event){
 			// 初始化记录参数
@@ -53,6 +89,8 @@
 		},
 		_end:function(event){
 			event.preventDefault();
+
+			this.endTime = until.getTime();
 			var moveSize = this.moveSize;
 			if(moveSize > 0) {
 				moveSize = 0;
@@ -81,6 +119,9 @@
 					this._end(event);
 					break;
 			}
+		},
+		refresh:function(){
+			this.endTime = 0;
 		}
 	};
 	// 移花接木
